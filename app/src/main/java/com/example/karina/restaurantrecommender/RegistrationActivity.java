3e33060d12a22972a50313ae2daf3116875fe237
Karina Kurmangaliyeva <kurmangaliyeva.karina@gmail.com>
2018-11-22 20:52:18 +0800

    user registration

--- app/src/main/java/com/example/karina/restaurantrecommender/RegistrationActivity.java
+++ app/src/main/java/com/example/karina/restaurantrecommender/RegistrationActivity.java
@@ -1,8 +1,6 @@
 package com.example.karina.restaurantrecommender;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,90 +8,61 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class RegistrationActivity extends AppCompatActivity {
-    EditText emailEditText, passwordEditText, phoneEditText, addressEditText;
-    SharedPreferences sharedPreferences;
-    String email = "";
+    EditText usernameEditText, emailEditText, passwordEditText, phoneEditText, addressEditText;
 
     public void completeRegistration(View view) {
-        email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
-        String phone = phoneEditText.getText().toString();
-        String address = addressEditText.getText().toString();
 
-        if (email.equals("") || phone.equals("") || phone.equals("") || address.equals("") ) {
-            Toast.makeText(this, "Please fill up your information", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            JSONObject profile = new JSONObject();
-            try {
-                profile.put("email", email);
-                profile.put("password", password);
-                profile.put("phoneNumber", phone);
-                profile.put("address", address);
+        if (usernameEditText.getText().toString().matches("")
+              || emailEditText.getText().toString().matches("")
+              || passwordEditText.getText().toString().matches("")
+              || phoneEditText.getText().toString().matches("")
+              || addressEditText.getText().toString().matches("")) {
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_LONG).show();
 
+        } else {
 
-            AsyncHttpClient client = new AsyncHttpClient();
+            ParseUser user = new ParseUser();
 
-            StringEntity entity = null;
-            try {
-                entity = new StringEntity(profile.toString());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+            user.setUsername(usernameEditText.getText().toString());
+            user.setEmail(emailEditText.getText().toString());
+            user.setPassword(passwordEditText.getText().toString());
+            user.put("phone", phoneEditText.getText().toString());
+            user.put("address", addressEditText.getText().toString());
 
-            JsonHttpResponseHandler responseHandler = new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    super.onSuccess(statusCode, headers, response);
+            user.signUpInBackground(new SignUpCallback() {
+                public void done(ParseException e) {
+                    if (e == null) {
 
-                    int accountId = -1;
-                    try {
-                        accountId = response.getInt("accountId");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    if(accountId == -1) {
-                        Log.i(" .. >> ", "could not get accountId");
-                        return;
-                    }
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
 
-                    sharedPreferences = RegistrationActivity.this.getSharedPreferences("com.example.karina.restaurantrecommender", Context.MODE_PRIVATE);
-                    sharedPreferences.edit().putString("email", email).apply();
-                    sharedPreferences.edit().putInt("accountId", accountId).apply();
+                    } else {
 
-                    startActivity(intent);
-                    finish();
-                }
+                        switch (e.getCode()) {
+                            case ParseException.USERNAME_TAKEN: {
 
-                @Override
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    super.onFailure(statusCode, headers, responseString, throwable);
-                    if (statusCode != 200) {
-                        Log.i(">>>", responseString);
-                        Toast.makeText(RegistrationActivity.this, "This email is already registered", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "This username is taken", Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                            case ParseException.EMAIL_TAKEN: {
+
+                                Toast.makeText(getApplicationContext(), "This email is already registered", Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                            default: {
+                                Log.i("Sign up", e.getMessage());
+                            }
+                        }
                     }
                 }
-            };
-
-            client.post(this, "http://shidfar.dlinkddns.com:8044/user/register", entity, "application/json",
-                  responseHandler);
+            });
         }
     }
 
@@ -102,9 +71,21 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        ParseUser.logOut();
+
+        setTitle("Registration Form");
+
+        usernameEditText = findViewById(R.id.usernameEditText);
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         phoneEditText = findViewById(R.id.phoneEditText);
         addressEditText = findViewById(R.id.addressEditText);
+
+        if (ParseUser.getCurrentUser() != null) {
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+
+        }
     }
 }
--- app/src/main/java/com/example/karina/restaurantrecommender/StarterApplication.java
+++ app/src/main/java/com/example/karina/restaurantrecommender/StarterApplication.java
@@ -1,4 +1,8 @@
+package com.example.karina.restaurantrecommender;
+
 import android.app.Application;
+
+import com.parse.Parse;
 
 public class StarterApplication extends Application {
 
@@ -6,6 +10,11 @@ public class StarterApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ed947120acc1c0631e772ea91f61abc7606c81e3")
+                .clientKey("0a464ce06a0b7f002888310cb2a153e6e3106ff4")
+                .server("http://13.58.163.206:80/parse")
+                .build()
+        );
     }
 }
